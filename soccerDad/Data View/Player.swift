03f6ae4100757df567f609320e4d8dcb7606c941
Ayse Kula <kula@ayse-kulas-macbook-pro.local>
2019-02-24 19:17:05 -0600

    Initial Commit

---
+++ soccerDad/Data View/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  soccerDad
+//
+//  Created by Ayse Kula on 2/24/19.
+//  Copyright © 2019 gokhan Kula. All rights reserved.
+//
+
+import Foundation
+
+class Player : Codable {
+    var name : String
+    var lastName : String
+    var number : Int
+    var skill : Int
+    
+}
